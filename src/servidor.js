240e315012b554b988fc91612411a7e5b75960ac
Sofia Vaz <sofiavazdacostaxavier@gmail.com>
2025-08-13 18:21:39 -0300

    feat: socket conecta usuário que entrou na página com o servidor

--- src/servidor.js
+++ src/servidor.js
@@ -1,6 +1,8 @@
 import express from "express"; 
 import url from "url"; 
 import path from "path"; 
+import http from "http"; 
+import { Server } from "socket.io"; 
 
 const app = express(); 
 const porta = process.env.porta || 3000; 
@@ -11,4 +13,10 @@ const diretorioPublico = path.join(caminhoAtual, "../..", "public");
 app.use(express.static(diretorioPublico)); 
 //o express vai usar o diretório público de forma estática
 
-app.listen(porta, () => console.log(`Servidor escutando na porta ${porta}`)); 
+const servidorHttp = http.createServer(app); 
+servidorHttp.listen(porta, () => console.log(`Servidor escutando na porta ${porta}`)); 
+
+const io = new Server(servidorHttp); 
+io.on("connection", () => {
+    console.log("Um cliente se conectou!"); 
+});  //escutar evento - on 
