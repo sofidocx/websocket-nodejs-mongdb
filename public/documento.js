4e2b4890ad9a78148f076dcc6338477a8f2041ae
Sofia Vaz <sofiavazdacostaxavier@gmail.com>
2025-08-14 14:10:15 -0300

    feat: adiciona comunicação simultânea no editor de texto

--- public/documento.js
+++ public/documento.js
@@ -1,1 +1,13 @@
 const socket = io(); 
+
+//trecho do html do qual a função quer localizar, no caso, o campo de texto do documento 
+const textoEditor = document.getElementById("editor-texto"); 
+
+textoEditor.addEventListener("keyup", () => {
+    socket.emit("texto_editor", textoEditor.value); 
+    //esse texto irá aparecer no devtools, o console do navegador 
+}); 
+
+socket.on("texto_editor_clientes", (texto) => {
+    textoEditor.value = texto; //atualiza o valor do campo de texto no campo do documento 
+}); 
--- src/socket-back.js
+++ src/socket-back.js
@@ -1,7 +1,10 @@
 import io from "./servidor.js";
 
-
-
 io.on("connection", (socket) => {
     console.log("Um cliente se conectou! ID: ", socket.id); //id do cliente que esta se conectando 
+//para cada cliente, iremos escutar os eventos 
+    socket.on("texto_editor", (texto) => {
+        //enviar esse evento para todos os clientes menos para o já conectado nesse socket 
+        socket.broadcast.emit("texto_editor_clientes", texto);
+    }); 
 });  //escutar evento - on 
